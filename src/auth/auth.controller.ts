2b3dacb39ceedc7e51f20f1eee387f7c8ef37092
Madesh-k <madesh.kcse@gmail.com>
2024-06-28 18:02:37 +0530

    Basic singupand signin in nestjs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authServie: AuthService) {}
+    constructor(private authService: AuthService) {}
      
     @Post('signup')
-    signup(){
-        return this.authServie.signup();
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup(dto);
     }
 
     @Post('Signin')
-    signin(){
-        return this.authServie.signin();
+    signin(@Body() dto: AuthDto){
+        return this.authService.signin(dto);
     }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
 
+
 @Module({})
-export class UseModule{}
+export class UserModule {
+}
